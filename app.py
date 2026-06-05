83722b5f33522ab4ff95d7dec702eac12cc33560
gokulraj-5 <2k5gokulraj@gmail.com>
2026-06-05 10:25:06 +0530

    Add Python 3.11 version file for Streamlit Cloud

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# Fix SQLite version for ChromaDB on Streamlit Cloud
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # -*- coding: utf-8 -*-
 import os
 import re
